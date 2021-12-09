f43eca4af6935086a9d33d717737e24a3bceae67
peixingzhe <peixingzhe@163.com>
2021-12-09 23:09:39 +0800

    zz

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,43 @@
 <template>
-  <h1>{{name}}</h1>
-  <h1>{{age}}</h1>
-  <button @click="sayHello">sayHello</button>
+  <h1 v-show="person.name">{{person.name}}</h1>
+  <h1>{{person.age}}</h1>
+  <h1>{{person.job}}</h1>
+  <h1 v-show="person.sex">{{person.sex}}</h1>
+  <h1>{{person.hobby}}</h1>
+  <button @click="changeJob">changeHobby</button>
+  <button @click="addSex">addSex</button>
+  <button @click="delName">delName</button>
 </template>
 
 <script>
-
+import {ref, reactive} from 'vue'
 export default {
   name: 'App',
   setup() {
+    let person = reactive({
+      name: '张三',
+      age: 18,
+      job: 'java开发',
+      hobby: ['抽烟', '喝酒', '烫头']
+    })
 
-    let name = '张三'
-    let age = 18
-
-    function sayHello() {
-      alert(`hello,我是${name},我今年${age}岁了！`)
-    };
+    function changeJob() {
+      person.hobby[0] = '吃饭'
+    }
+    function addSex() {
+      person.sex = '男'
+    }
+    function delName() {
+      console.log(person)
+      delete person.name
+      console.log(person)
+    }
 
     return {
-      name,
-      age,
-      sayHello
+      person,
+      changeJob,
+      addSex,
+      delName
     }
   }
 }
